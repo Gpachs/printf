89f2ba3e2b32560f2e588a69a05c64d74a02a91d
Gpachs <obiomagoldman@gmail.com>
2022-07-15 06:11:24 -0700

    file update

--- 3-printf.c
+++ 3-printf.c
@@ -33,11 +33,11 @@ int print_int(va_list ap, params_t *params)
 	long l;
 
 	if (params->l_modifier)
-	l = va_arg(ap, long);
+		l = va_arg(ap, long);
 	else if (params->h_modifier)
-	l = (short int)va_arg(ap, int);
+		l = (short int)va_arg(ap, int);
 	else
-	l = (int)va_arg(ap, int);
+		l = (int)va_arg(ap, int);
 	return (print_number(convert(l, 10, 0, params), params));
 }
 
